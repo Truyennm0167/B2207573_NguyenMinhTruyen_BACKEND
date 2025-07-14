c2f48eb75a0886d8870508ef8f4a56cfa6b0ec2e
Truyennm0167 <truyentruyen113113@gmail.com>
2025-07-14 22:41:44 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
@@ -12,4 +12,17 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
 
+// Handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+// Define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 module.exports = app;
+
